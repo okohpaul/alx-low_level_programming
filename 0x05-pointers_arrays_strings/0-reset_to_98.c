d9da7c01286e0ba56a2eac6090105579727b0791
okohpaul <hommie4real@gmail.com>
2022-09-19 13:39:35 -0700

    trial

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,18 @@
 #include "main.h"
 
+/**
+*
+
+/**
+*void reset_to_98 takes a pointer to an int as parameter and updates the value it points to to 98.
+*
+*/
 
 
-#include "main.h"
+void reset_to_98(int *n)
+{
+       int n = 98;
+       int *ip;
+      _putchar(*ip);
+     return (0);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,7 +8,6 @@
 * Desc: Header file containing declarations for all functions
 */
 
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -19,6 +18,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
 
 #endif
