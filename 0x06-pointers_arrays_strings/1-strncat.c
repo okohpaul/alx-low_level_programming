378f4ff426cac7979376c8de5df910c10788ff89
okohpaul <hommie4real@gmail.com>
2022-09-22 17:43:34 -0700

    1 task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,37 +1,33 @@
-#include "main.h"
+include "main.h"
 
 /**
- *  * *_strcat - function commute srtings
- *    @dest: param pointer to a char
- *    @src: param pointer to a char
- *    @n: the limit of the concatenation 
- *    Return: return value of dest
+ * _strncat - two words
+ * @dest : pointer to char param
+ * @src : pointer to char param
+ * @n : int parameter
+ * Return: *dest
  */
 
-
- char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-		int m;
-			int i;
+	int m;
+	int i;
 
-				m = 0;
+	m = 0;
 
-					for (i = 0; i < 1000; i++)
-							{
-										if (dest[i] == '\0')
-													{
-																	break;
-																			}
-												m++;
-													}
+	for (i = 0; i < 1000; i++)
+	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		m++;
+	}
 
-						for (i = 0; src[i] != '\0' && i < n; i++)
-								{
-											dest[m + i] = src[i];
-												}
-							dest[m + i] = '\0';
-								return (dest);
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m + i] = '\0';
+	return (dest);
 }
-
-
-
