78296f98d6be3368c1fc1e2603226619a30d5d72
okohpaul <hommie4real@gmail.com>
2022-09-22 18:01:14 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
 /**
- * string_toupper - lower to upper
+ * *cap_string - this is awesome
  * @s: pointer to char params
  *
  * Return: *s
  */
 
-char *string_toupper(char *s)
+char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+	char delimeters[] = " \t\n,;.!?\"(){}";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] = s[0] - 32;
+				for (j = 0; delimeters[j] != '\0'; j++)
+					if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
 	}
 	return (s);
 }
